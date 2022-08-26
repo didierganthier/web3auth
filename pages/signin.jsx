b672c6bc5ac83b62752d44abe124c740ecddf52c
Didier Peran Ganthier <didierganthierperan@gmail.com>
2022-08-26 11:15:59 -0400

    Create Sign in Page

---
+++ pages/signin.jsx
@@ -0,0 +1,23 @@
+import { InjectedConnector } from "@wagmi/core";
+import { useConnect } from "wagmi";
+
+function SignIn() {
+    const { connectAsync } = useConnect();
+
+    const handleAuth = async () => {
+        const { account, chain } = await connectAsync({ connector: new InjectedConnector() });
+
+        const userData = { address: account, chain: chain.id, network: 'evm' };
+
+        console.log(userData);
+    }
+
+    return (
+        <div>
+            <h3>Web3 Authentication</h3>
+            <button onClick={() => handleAuth()}>Authenticate via Metamask</button>
+        </div>
+    );
+}
+
+export default SignIn;
